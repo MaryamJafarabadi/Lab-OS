a25908e4e43205bc0f55b5425a04be2f63c1db95
SanaNavaei <110608046+SanaNavaei@users.noreply.github.com>
2022-11-12 19:11:31 +0330

    Update syscall.c

--- LAB_2/syscall.c
+++ LAB_2/syscall.c
@@ -105,6 +105,7 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_find_largest_prime_factor(void);
 extern int sys_get_parent_pid(void);
+extern int sys_change_file_size(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -130,6 +131,7 @@ static int (*syscalls[])(void) = {
 [SYS_close]   sys_close,
 [SYS_find_largest_prime_factor]   sys_find_largest_prime_factor,
 [SYS_get_parent_pid]     sys_get_parent_pid ,
+[SYS_change_file_size]   sys_change_file_size,
 };
 
 void
