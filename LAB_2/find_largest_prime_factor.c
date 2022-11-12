37af064163c932b73feb8222416652b90cf07bda
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2022-11-12 11:37:20 +0330

    Update find_largest_prime_factor.c
    Adding some messages for detecting the system-calls process.

--- LAB_2/find_largest_prime_factor.c
+++ LAB_2/find_largest_prime_factor.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[])
     int saved_reg;
     asm ("movl %%ebp , %0;" :"=r" (saved_reg) ::);
     asm ("movl %0, %%ebp;" :: "r"(atoi(argv[1])):);
+    printf(1,"We are in user mode and the system call of finding the largest prime factor is being called!\n");
     int result = find_largest_prime_factor();
     if (result == -2) printf(1, "Please enter positive number.\n");
     else if (result == -1) printf(1,"This number has no prime factor.\n");
