734d55406deca47dcb4119c2bbfe5fe8ca6b8cb6
MaryamJafarabadi <97696809+MaryamJafarabadi@users.noreply.github.com>
2022-11-13 18:34:16 +0330

    Update syscall.c
    Adding new system-call get_callers.

--- LAB_2/syscall.c
+++ LAB_2/syscall.c
@@ -106,6 +106,7 @@ extern int sys_uptime(void);
 extern int sys_find_largest_prime_factor(void);
 extern int sys_get_parent_pid(void);
 extern int sys_change_file_size(void);
+extern int sys_get_callers(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -132,6 +133,7 @@ static int (*syscalls[])(void) = {
 [SYS_find_largest_prime_factor]   sys_find_largest_prime_factor,
 [SYS_get_parent_pid]     sys_get_parent_pid ,
 [SYS_change_file_size]   sys_change_file_size,
+[SYS_get_callers]   sys_get_callers,
 };
 
 void
@@ -142,6 +144,7 @@ syscall(void)
 
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    add_this_pid(num, curproc->pid);
     curproc->tf->eax = syscalls[num]();
   } else {
     cprintf("%d %s: unknown sys call %d\n",
